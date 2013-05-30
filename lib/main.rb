c9e09ef6a40778ffec88a4c9a99e1ed8d6c10ff2
Patrick Plenefisch <simonpatp@gmail.com>
2013-05-30 17:54:06 -0400

    Properly expanding paths

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,7 @@
 require 'jrubyfx'
 #p Java::javafx.beans.property.SimpleStringProperty
 $LOAD_PATH << "."
-$PLUGIN_DIR = File.join(File.dirname(File.dirname(__FILE__)), "plugins")
+$PLUGIN_DIR = File.join(File.dirname(File.dirname(File.expand_path __FILE__)), "plugins")
 q = $LOAD_PATH.find { |i| i.include?(".jar!/META-INF/jruby.home/lib/ruby/")}
 if q
   xx = File.dirname(q[0..(2 + q.index(".jar!/META-INF/jruby.home/lib/ruby/"))]).gsub(/^file\:/, '')
