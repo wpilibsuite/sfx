9b95fc5280993a5f6c3a122f11b14fe43a6ebf4c
Patrick Plenefisch <simonpatp@gmail.com>
2013-06-04 09:30:30 -0400

    Adding simple code editor

---
+++ lib/designer_support/code_editor.rb
@@ -0,0 +1,57 @@
+# Copyright (C) 2013 patrick
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module SD
+  module DesignerSupport
+    class CodeEditor
+      include JRubyFX::Controller
+      fxml "CodeEditor.fxml"
+
+      def initialize(blk)
+        @blk = blk
+      end
+
+      def code
+        @code_area.text
+      end
+
+      def code=(value)
+        @code_area.text = value
+      end
+
+      def ok
+        @blk.call(code)
+        @stage.hide
+      end
+
+      def cancel
+        @blk.call(false)
+        @stage.hide
+      end
+
+
+      def self.show_and_wait(stg)
+        res = nil
+        blk = lambda{|result|res = result}
+        stage(init_style: :utility, init_modality: :app, title: "SmartDashboard Settings") do
+          init_owner stg
+          fxml SD::SettingsDialog, :initialize => [blk]
+          show_and_wait
+        end
+        res
+      end
+    end
+  end
+end
