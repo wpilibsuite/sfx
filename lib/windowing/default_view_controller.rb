a97627ac0d367c38a8934714da109efb67d0eeb6
Patrick Plenefisch <simonpatp@gmail.com>
2013-06-27 10:14:52 -0400

    De-annoying sam

--- lib/windowing/default_view_controller.rb
+++ lib/windowing/default_view_controller.rb
@@ -19,7 +19,7 @@ module SD
       attr_accessor :tab, :name
       attr_reader :layout_manager
       def initialize
-        @name = "general"
+        @name = "General"
         self.root_canvas = SD::DesignerSupport::Preferences.root_canvas.new
         # load default prefs
         @filter = SD::DesignerSupport::AAFilter.new
