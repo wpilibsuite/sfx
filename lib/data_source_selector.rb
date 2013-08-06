26ded04f221480e5cb696231e4cdb18955885786
Patrick Plenefisch <simonpatp@gmail.com>
2013-08-06 11:20:09 -0400

    Fixing data source selector

--- lib/data_source_selector.rb
+++ lib/data_source_selector.rb
@@ -12,6 +12,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+require 'utils/url'
+
 module SD
   class DataSourceSelector
     include JRubyFX::Controller
@@ -21,56 +23,60 @@ module SD
 
     def initialize(core)
       @core = core
+      # TODO: clean this up
       @all_combos = {
         :in => [@root_source],
         #:out => [@root_sink]
       }
       @combo_infos = {
         :in => [{selected: 0, items: ["None"]}],
-       # :out => [{selected: 0, items: ["None"]}],
+        # :out => [{selected: 0, items: ["None"]}],
       }
-      @combo_taints = []
-      @combo_bottoms = []
+      HBox.setHgrow(@panes, Priority::ALWAYS) # silly this cant be in FXML
+
+      # input sanitizations, these should be outside so we can do prefs also
       epts = @core.all_data_endpoints
-      ep = epts[0]
+      url = SD::Utils::Url.from(epts[0])
+      puts url
+      # TODO: evil
       [@root_source].each do |cb| # root_sink
         combo_init cb
       end
       # TODO: should we use bindings?  yes, except the combo box is special...
-      combo_add :current, ep, :in, 0
-      @name.text_property.bind_bidirectional ep.name_property
-      @old_np = ep.name_property
-      @name.set_on_action &method(:update_combo_view)
-      @host.text = ep.init_info.host
-      annote = ep.object.java_class.annotation(Java::dashfx.lib.controls.DesignableData.java_class)
-      @type_info.text = "#{annote.name}\n#{annote.description}"
-      @type_classname.text = ep.object.java_class.inspect.sub(/^class /, '')
+      @root_source.items.clear
+      @root_source.items.add(url)
+      @root_source.selection_model.clear_and_select(0)
+      load_info_pane url
+      @all_data_sources.items.clear
       # Search for all types
       Java::dashfx.lib.registers.DataProcessorRegister.get_all.each do |e|
-        combo_add :new_type, [e.annotation(Java::dashfx.lib.controls.DesignableData.java_class), e]
+        @all_data_sources.items.add DSSMenuItem.new(DataBuilder.new(e)) {|db| create_url(db)}
       end
-      combo_flush
+      # always show the current team-number induced value
+      @host.prompt_text = Java::dashfx.lib.data.InitInfo.new.host
     end
 
-    def load_info_pane(did)
-      @name.text_property.unbind_bidirectional @old_np
-      @name.text_property.bind_bidirectional did.name_property
-      @old_np = did.name_property
-      @host.text = did.init_info.host
-      annote = did.object.java_class.annotation(Java::dashfx.lib.controls.DesignableData.java_class)
-      @type_info.text = "#{annote.name}\n#{annote.description}"
-      @type_classname.text = did.object.java_class.inspect.sub(/^class /, '')
-    end
-
-    def update_combo_view(e)
-      # TODO: its probbably better to search for the ones that need redrawing and force just that.
-      @all_combos.each do |k, its|
-        its.each do |combo|
-          ix = combo.selection_model.selected_index
-          combo.selection_model.clear_and_select 0
-          combo.selection_model.clear_and_select ix
+    def create_url(db)
+      url = db.new
+      @all_combos.each do |io, vals|
+        vals.each do |cb|
+          cb.items << url
         end
       end
+      @root_source.selection_model.select_last
+    end
+
+    def load_info_pane(url)
+      if @old_url
+        @name.text_property.unbind_bidirectional @old_url.name_property
+        @host.text_property.unbind_bidirectional @old_url.host_property
+      end
+      @name.text_property.bind_bidirectional url.name_property
+      @host.text_property.bind_bidirectional url.host_property
+      annote = url.find_class.java_class.annotation(Java::dashfx.lib.controls.DesignableData.java_class)
+      @type_info.text = "#{annote.name}\n#{annote.description}"
+      @type_classname.text = url.class_name
+      @old_url = url
     end
 
     def combo_init(combo)
@@ -80,82 +86,14 @@ module SD
       combo.button_cell = SD::DSSListCell.new
       combo.value_property.add_change_listener do
         next if @ignore_it
-        if @smash_quiet #TODO: why do I need to do this?
-          @smash_quiet.call
-          @smash_quiet = nil
-        end
 
-        item = combo.value
-        if item == :seperator
-          loc = :in
-          idx = @all_combos[loc].index(combo)
-          # TODO: figure out strange sections
-          combo_select @combo_infos[loc][idx][:selected], loc, idx
-          combo_flush
-        elsif item.is_a? Array and item[0].kind_of? Java::dashfx.lib.controls.DesignableData
-          job = item[1].ruby_class.new
-          did = DataInitDescriptor.new(job, item[0].name.match(/[\:]?([^\:]*)$/)[1], InitInfo.new(), nil)
-          # TODO: check if its in/out, just out, or just in
-          @all_combos[:in].length.times do |idx|
-            combo_add (@all_combos[:in][idx] == combo ? :current : :possible), did, :in, idx
-          end
-          combo_flush
-        elsif item.is_a? String
-          # nothing exciting
-        elsif item # TODO: don't assume
-          # reload the info pane
-          load_info_pane item
-        end
+        load_info_pane combo.value
       end
-    end
-
-    def combo_flush
-      if @combo_taints.include? :all
-        # TODO: may not be square
-        @combo_taints = ([:in].product (0...@combo_infos[:in].length).to_a) # TODO: out
-      end
-      @combo_taints.each do |comb, idx|
-        combo = @all_combos[comb][idx]
-        data = @combo_infos[comb][idx]
-        @ignore_it = true
-        @smash_quiet = Proc.new do
-                  combo.selection_model.select combo.items[data[:selected]]
-        end
-        combo.items.clear
-        combo.items.add_all data[:items]
-        combo.items.add :seperator
-        combo.items.add_all @combo_bottoms
-        combo.selection_model.select combo.items[data[:selected]]
-        @ignore_it = false
-      end
-      @combo_taints = []
-    end
-
-    def combo_add(type, value, combo=nil, indx=nil)
-      case type
-      when :current, :possible
-        cin = @combo_infos[combo][indx]
-        cin[:items] << value
-        cin[:selected] = (cin[:items].length - 1) if type == :current
-        @combo_taints << [combo, indx]
-      when :new_type
-        @combo_bottoms << value
-        @combo_taints << :all
-      else
-        puts "EEK! unknown combo add type #{type}"
-      end
-    end
-
-    def combo_select(selected, combo, indx)
-      @combo_infos[combo][indx][:selected] = selected
-      @combo_taints << [combo, indx]
     end
 
     def save_it
-      # FIXME: BUG: can't hit save on same items
       @core.clear_all_data_endpoints
-      @root_source.value.mount_point = @root_url.text
-      @core.mount_data_endpoint @root_source.value
+      @core.mount_data_endpoint @root_source.value.to_did #TODO: support more
       @stage.hide
     end
   end
@@ -164,23 +102,40 @@ module SD
     include JRubyFX
     def updateItem(item, empty)
       super
-      if (item != nil)
-        self.disabled = false
-        self.tooltip = nil
-        if item == :seperator
-          self.text = "------------"
-          self.disabled = true
-        elsif item.is_a? Array and item[0].kind_of? Java::dashfx.lib.controls.DesignableData
-          self.text = "New #{item[0].name.match(/[\:]?([^\:]*)$/)[1]}"
-          self.tooltip = build(Tooltip, item[1].name)
-        elsif item.is_a? String
-          self.text = item
-        else # TODO: don't assume
-          self.text = item.name
-        end
+      if item
+        text_property.bind item.name_property
       else
+        text_property.unbind
         self.text = nil
       end
+    end
+  end
+
+  class DSSMenuItem < Java::JavafxSceneControl::MenuItem
+    def initialize(db, &on_action)
+      super()
+      self.text = db.name
+      @db = db
+      on_action {|e| on_action.call(db, e)}
+    end
+  end
+
+  class DataBuilder
+    def initialize(e)
+      @rclass = e.ruby_class
+      @annote = e.annotation(Java::dashfx.lib.controls.DesignableData.java_class)
+    end
+    def new
+      SD::Utils::Url.new("New #{name}", @rclass, nil, nil, "/")
+    end
+    def name
+      @annote.name
+    end
+    def description
+      @annote.description
+    end
+    def to_s
+      @annote.to_s
     end
   end
 end
---
+++ lib/utils/url.rb
@@ -0,0 +1,72 @@
+# Copyright (C) 2013 patrick
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+require 'cgi'
+
+module SD
+  module Utils
+    class Url
+      include JRubyFX::DSL
+      property_accessor :name, :class_name, :host, :port, :mount, :options
+
+      URL_REGEX = /dashfx:\/\/(?<class_name>[\w\.:]+)@(?<host>[^:]*)(:(?<port>[0-9]*))?(?<mount>\/([\w\+%]*))(\?(?<args>.*))?/
+      MAGIC_HOST = "xn--mxam4bc95czbc7b6hubbgd"
+
+      def initialize(name, clazz, host, port, mount, options={})
+        @name = simple_string_property(self, "name", name)
+        @class_name = simple_string_property(self, "className", clazz.to_s)
+        @host = simple_string_property(self, "host", (host.nil? or host.empty? or host == MAGIC_HOST) ? nil : host)
+        @port = simple_integer_property(self, "port", (port.nil? or port.empty?) ? 0 : port.to_i)
+        @mount = simple_string_property(self, "mount", mount)
+      end
+
+      def find_class
+        unless @saved_class
+          @saved_class = eval(class_name) # TODO: use constantize
+        end
+        @saved_class
+      end
+
+      def to_did
+        iii = build(Java::dashfx.lib.data.InitInfo, host: host, port: port)
+        # TODO: options
+        Java::dashfx.lib.data.DataInitDescriptor.new(find_class.new, name, iii, mount)
+      end
+
+      def to_s
+        @str = "dashfx://#{class_name}@#{(host.nil? or host.empty?) ? MAGIC_HOST : host}:#{port}#{mount}"
+        unless true # @options.empty?
+          @str << "?#{@options.map{|x| raise "TODO"}.join("&")}"
+        end
+        return @str
+      end
+
+      def self.parse(url, name=nil)
+        m = URL_REGEX.match(url)
+        # TODO: parse options
+        new(name || m[:class_name].split(".").last, m[:class_name], m[:host], m[:port], m[:mount], m[:options])
+      end
+      def self.from(did)
+        name = did.name
+        obj = did.object
+        iii = did.init_info
+        mount = did.mount_point
+        port = iii.port
+        host = iii.raw_host
+        opts = iii.all_options
+        new name, obj.class, host, port, mount, opts
+      end
+    end
+  end
+end
