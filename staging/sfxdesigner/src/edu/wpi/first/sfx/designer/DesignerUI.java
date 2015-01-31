c71211f2bab865e4f93b5fb8e5ecf346a0f1cf57
Patrick Plenefisch <simonpatp@gmail.com>
2015-01-31 17:31:20 -0500

    Adding new designer project in staging
    --HG--
    branch : sfx.next

---
+++ staging/sfxdesigner/src/edu/wpi/first/sfx/designer/DesignerUI.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2015 patrick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.wpi.first.sfx.designer;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+
+/**
+ *
+ * @author patrick
+ */
+public class DesignerUI
+{
+
+	@FXML
+	private ScrollPane spain;
+
+	@FXML
+	private Tooltip aa_regex_message;
+
+	@FXML
+	private BorderPane BorderPane;
+
+	@FXML
+	private Label alert_msg;
+
+	@FXML
+	private HBox aa_ctrl_regex;
+
+	@FXML
+	private VBox left_gutter;
+
+	@FXML
+	private Button stop_button;
+
+	@FXML
+	private Image add_tab_plus;
+
+	@FXML
+	private HBox tab_box;
+
+	@FXML
+	private TreeView<?> AATreeview;
+
+	@FXML
+	private AnchorPane overlay_pod;
+
+	@FXML
+	private HBox bottom_gutter;
+
+	@FXML
+	private Button aa_expand_panel;
+
+	@FXML
+	private SplitMenuButton open_btn;
+
+	@FXML
+	private TextField aa_regexer;
+
+	@FXML
+	private GridPane GridPane;
+
+	@FXML
+	private Button playback_button;
+
+	@FXML
+	private ImageView add_tab_icon;
+
+	@FXML
+	private Accordion accord;
+
+	@FXML
+	private HBox add_slider;
+
+	@FXML
+	private Pane west_wing;
+
+	@FXML
+	private Pane south_wing;
+
+	@FXML
+	private Pane gutter_shadow;
+
+	@FXML
+	private Pane east_wing;
+
+	@FXML
+	private Pane hack_corner;
+
+	@FXML
+	private Pane north_wing;
+
+	@FXML
+	private VBox aa_ctrl_panel;
+
+	@FXML
+	private TitledPane x2;
+
+	@FXML
+	private HBox msg_carrier;
+
+	@FXML
+	void canvas_keyup(KeyEvent event)
+	{
+
+	}
+
+	@FXML
+	void show_toolbox(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void new_document(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void open(ActionEvent event)
+	{
+
+	}
+
+
+	@FXML
+	void save(ActionEvent event)
+	{
+
+	}
+
+
+	@FXML
+	void save_as(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void new_tab(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void edit_settings(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void run(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void drag_drop(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void drag_over(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void canvas_click(MouseEvent event)
+	{
+
+	}
+
+	@FXML
+	void surrender_nest(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void design(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void do_playback_mode(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void aa_add_all(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void aa_toggle_panel(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void show_data_sources(ActionEvent event)
+	{
+
+	}
+
+	@FXML
+	void hide_toolbox(ActionEvent event)
+	{
+
+	}
+}
---
+++ staging/sfxdesigner/src/edu/wpi/first/sfx/designer/MainController.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.sfx.designer;
+
+import dashfx.controls.DataAnchorPane;
+import dashfx.lib.registers.ControlRegister;
+import dashfx.lib.util.CTView;
+import dashfx.lib.util.ControlTree;
+import dashfx.lib.util.DemoImpl;
+import dashfx.lib.util.DesignerMetadata;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.*;
+
+/**
+ * load empty tree model
+ * load jruby
+ * load plugins
+ * load controls
+ * 
+ * 
+ * 
+ */
+
+/**
+ *
+ * @author patrick
+ */
+public class MainController implements Initializable
+{
+
+	@FXML
+	private TreeView tree;
+	@FXML
+	private SplitPane par;
+	@FXML
+	private ListView<Class> list;
+	private ControlTree ti;
+	private ObservableList<Class> listi;
+	private DemoImpl infl;
+	private CTView aCanvas;
+	private DesignerMetadata meta;
+	
+	@FXML
+	void odd(DragEvent e)
+	{
+		System.out.println("completes! " + this.toString() + " .. " + e.toString() + " ??? " + e.isDropCompleted());
+		tree.setRoot(remap(ti)); // TODO: observable
+	}
+
+	@FXML
+	void grabItem2(MouseEvent event)
+	{
+		Dragboard db = list.startDragAndDrop(TransferMode.COPY);
+		ClipboardContent content = new ClipboardContent();
+		content.put(DataFormat.lookupMimeType("application/x-java-class"), list.getSelectionModel().getSelectedItem());
+		db.setContent(content);
+		event.consume();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb)
+	{
+		infl = new DemoImpl();
+		try
+		{
+			meta = new DesignerMetadata(DataAnchorPane.class, infl, new URL("file:/home/patrick/NetBeansProjects/sfx/lib/res/DesignerOverlayControl.fxml")); // the root
+		}
+		catch (MalformedURLException ex)
+		{
+			throw new RuntimeException(ex);
+		}
+		listi = FXCollections.observableArrayList(ControlRegister.getAll());
+		list.setItems(listi);
+		ti = meta.getRoot();
+		tree.setRoot(remap(ti));
+		par.getItems().add(1, meta.getAssociatedView(ti));
+		infl.runService();
+	}
+
+	private TreeItem<String> remap(ControlTree ti)
+	{
+		TreeItem<String> treeItem = new TreeItem<>(ti.getControlType().getName());
+		treeItem.getChildren().addAll(ti.getChildren().stream().map(child -> remap(child)).collect(Collectors.toList()));
+		return treeItem;
+	}
+
+}
